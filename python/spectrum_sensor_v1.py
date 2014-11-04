fa13f7dd7e2d8ea9cfcf3818c6008e27abbe7a45
Germano <germanocapela@gmail.com>
2014-11-04 11:39:58 +0000

    commented code and optimizations

--- python/spectrum_sensor_v1.py
+++ python/spectrum_sensor_v1.py
@@ -4,7 +4,8 @@
 # Copyright 2014 germanocapela at gmail dot com
 # spectrum sensor - multichannel energy detector
 # log the psd after a peak hold
-# 
+# log maximum power per channel
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
@@ -33,37 +34,42 @@ import pmt
 import threading, os
 from os.path import expanduser
 
-from ofdm_cr_tools import frange, fast_spectrum_scan, movingaverage
+from ofdm_cr_tools import frange, movingaverage
 
 #log files information
 
 start_dat = time.strftime("%y%m%d")
 start_tim = time.strftime("%H%M%S")
 
+#create a folder with date and time in user system folder
 home = expanduser("~")
 directory = home+'/sensing' + '-' + start_dat + '-' + time.strftime("%H%M") + '/'
-
 if not os.path.exists(directory):
 	os.makedirs(directory)
 
+#initialize path for cumulative files
 path_cumulative_psd_log = directory+'sdr_psd_cumulative_log' + '-' + start_dat + '-' + start_tim + '.matz'
 path_cumulative_stat_log = directory+'sdr_ss_cumulative_log' + '-' + start_dat + '-' + start_tim + '.log'
 path_cumulative_max_power_log = directory+'sdr_max_power_cumulative_log' + '-' + start_dat + '-' + start_tim + '.matz'
 
-periodicity = 60*60 #log files periodicity
+#global variable for periodicity of periodic logs
+periodicity = 60*60
 
+#initialize path for periodic files and periodic variables
 periodic_psd_peaks = None
 path_periodic_psd_log = directory+'sdr_psd_periodic_log' + '-' + start_dat + '-' + start_tim + '.matz'
 periodic_statistic = {}
-n_measurements_period = 0
+n_measurements_period = 0 #reset counter for periodic measurements
 path_periodic_stat_log = directory+'sdr_ss_periodic_log' + '-' + start_dat + '-' + start_tim + '.log'
 periodic_max_powers = None
 path_periodic_max_powers_log = directory+'sdr_max_power_periodic_log' + '-' + start_dat + '-' + start_tim + '.matz'
 
+#function to set periodicity upon flowgraph construction
 def set_log_periodicity(period):
 	global periodicity
 	periodicity = period
 
+#function that starts the periodic procedure of renewing periodic log files
 def periodic_files():
 	global path_periodic_psd_log, path_periodic_stat_log, path_periodic_max_powers_log, periodic_psd_peaks, periodic_statistic, periodic_max_powers, n_measurements_period
 
@@ -88,16 +94,15 @@ class spectrum_sensor_v1(gr.hier_block2):
 			"spectrum_sensor_v1",
 			gr.io_signature(1, 1, gr.sizeof_gr_complex),
 			gr.io_signature(0,0,0))
-		self.fft_len = fft_len
-		self.sens_per_sec = sens_per_sec
-		self.sample_rate = sample_rate
-		self.channel_space = channel_space
-		self.search_bw = search_bw
-		self.thr_leveler = thr_leveler
-		self.tune_freq = tune_freq
-		self.threshold = 0
-		self.power_level_ch = []
-		self.alpha_avg = alpha_avg
+		self.fft_len = fft_len #lenght of the fft for spectral analysis
+		self.sens_per_sec = sens_per_sec #number of measurements per second (decimates)
+		self.sample_rate = sample_rate 
+		self.channel_space = channel_space #channel space for analysis
+		self.search_bw = search_bw #search bandwidth within each channel
+		self.thr_leveler = thr_leveler #leveler factor for noise floor / threshold comparison
+		self.tune_freq = tune_freq #center frequency
+		self.threshold = 0 #actual value of the threshold
+		self.alpha_avg = alpha_avg #averaging factor for noise level between consecutive measurements
 
 		self.msgq0 = gr.msg_queue(2)
 		self.msgq1 = gr.msg_queue(2)
@@ -128,6 +133,7 @@ class spectrum_sensor_v1(gr.hier_block2):
 		set_log_periodicity(period)
 		periodic_files()
 
+#queue wathcer to log max psd
 class _queue1_watcher(_threading.Thread):
 	def __init__(self, rcvd_data, verbose):
 		_threading.Thread.__init__(self)
@@ -159,17 +165,17 @@ class _queue1_watcher(_threading.Thread):
 			payload = msg.to_string()
 			complex_data = np.fromstring (payload, np.float32)
 
-			#cumulative
+			#cumulative log
 			cumulative_psd_peaks = np.maximum(complex_data, cumulative_psd_peaks) 
 			self.psd_mat_file = open(path_cumulative_psd_log,'w')
 			np.save(self.psd_mat_file, cumulative_psd_peaks)
 
-			#periodic
+			#periodic log
 			periodic_psd_peaks = np.maximum(complex_data, periodic_psd_peaks) 
 			periodic_psd_log = open(path_periodic_psd_log,'w')
 			np.save(periodic_psd_log, periodic_psd_peaks)
 
-
+#queue wathcer to log statistics and max power per channel
 class _queue0_watcher(_threading.Thread):
 	def __init__(self, rcvd_data, sens_per_sec, tune_freq, channel_space,
 		 search_bw, fft_len, sample_rate, thr_leveler, alpha_avg, test_duration, trunc_band, verbose):
@@ -178,6 +184,8 @@ class _queue0_watcher(_threading.Thread):
 		self.rcvd_data = rcvd_data
 		self.log_stat_file = open(path_cumulative_stat_log,'w')
 		print 'successfully created log_stat_file', self.log_stat_file
+		self.max_power_file = open(path_cumulative_max_power_log,'w')
+		print 'successfully created max_power_log', self.max_power_file
 
 		self.tune_freq = tune_freq
 		self.channel_space = channel_space
@@ -190,14 +198,22 @@ class _queue0_watcher(_threading.Thread):
 		self.trunc = sample_rate-trunc_band
 		self.trunc_ch = int(self.trunc/self.channel_space)/2
 		
+		#test general settings
 		self.settings = {'date':time.strftime("%y%m%d"), 'time':time.strftime("%H%M%S"), 'tune_freq':tune_freq, 'sample_rate':sample_rate, 'fft_len':fft_len,'channel_space':channel_space, 'search_bw':search_bw, 'test_duration':test_duration, 'sens_per_sec':sens_per_sec, 'n_measurements':0, 'noise_estimate':self.noise_estimate, 'trunc_band':trunc_band, 'thr_leveler':thr_leveler}
+		#test statistics
 		self.statistic = {}
-		self.log_stat_file.write('settings ' + str(self.settings) + '\n')
-		self.log_stat_file.write('statistics ' + str(self.statistic) + '\n')
 
-		self.max_power_file = open(path_cumulative_max_power_log,'w')
-		print 'successfully created max_power_log', self.max_power_file
+		#test max power per channel
 		self.max_powers = None
+
+		self.Fr = float(self.sample_rate)/float(self.fft_len) #freq resolution
+		self.Fstart = self.tune_freq - self.sample_rate/2 #start freq
+		self.Ffinish = self.tune_freq + self.sample_rate/2 #end freq
+		self.bb_freqs = frange(-self.sample_rate/2, self.sample_rate/2, self.channel_space) #baseband freqs
+		self.srch_bins = self.search_bw/self.Fr #binwidth for search
+		self.ax_ch = frange(self.Fstart, self.Ffinish, self.channel_space) #subject channels
+		if self.trunc > 0:
+			self.ax_ch = self.ax_ch[self.trunc_ch:-self.trunc_ch] #trunked subject channels
 
 		self.verbose = verbose
 		self.keep_running = True
@@ -216,27 +232,30 @@ class _queue0_watcher(_threading.Thread):
 				start = itemsize * (nitems - 1)
 				s = s[start:start+itemsize]
 
+			#convert received data to numpy vector
 			payload = msg.to_string()
 			complex_data = np.fromstring (payload, np.float32)
+			#scann channels
 			spectrum_constraint_hz = self.spectrum_scanner(complex_data)
+			#count cumulative measurements
 			self.settings['n_measurements'] += 1
+			#count periodic measurements
 			n_measurements_period += 1
 			self.settings['n_measurements_period'] = n_measurements_period
+			#get noise estimate
 			self.settings['noise_estimate'] = self.noise_estimate
 
 			#register data/time
 			self.settings['date'] = time.strftime("%y%m%d")
 			self.settings['time'] = time.strftime("%H%M%S")
 
-			#count occurrences - cumulative
 			for el in spectrum_constraint_hz:
+				#count occurrences -> cumulative
 				if el in self.statistic:
 					self.statistic[el] += 1
 				else:
 					self.statistic[el] = 1
-
-			#count occurrences - periodic
-			for el in spectrum_constraint_hz:
+				#count occurrences -> periodic
 				if el in periodic_statistic:
 					periodic_statistic[el] += 1
 				else:
@@ -260,23 +279,16 @@ class _queue0_watcher(_threading.Thread):
 				#print 'settings', self.settings
 				print 'statistics', self.statistic
 
+	#function that scans channels and compares with threshold to determine occupied / not occupied
 	def spectrum_scanner(self, samples):
 		global periodic_max_powers
 
-		Fr = float(self.sample_rate)/float(self.fft_len)
-		Fstart = self.tune_freq - self.sample_rate/2
-		Ffinish = self.tune_freq + self.sample_rate/2
+		#measure power for each channel
+		power_level_ch = src_power(samples, self.fft_len, self.Fr, self.sample_rate, self.bb_freqs, self.srch_bins)
 
-		bb_freqs = frange(-self.sample_rate/2, self.sample_rate/2, self.channel_space)
-		srch_bins = self.search_bw/Fr
-
-		#measure powers
-		power_level_ch = src_power(samples, self.fft_len, Fr, self.sample_rate, bb_freqs, srch_bins)
-		ax_ch = frange(Fstart, Ffinish, self.channel_space)
-
+		#trunc channels outside useful band (filter curve) --> trunc band < sample_rate
 		if self.trunc > 0:
 			power_level_ch = power_level_ch[self.trunc_ch:-self.trunc_ch]
-			ax_ch = ax_ch[self.trunc_ch:-self.trunc_ch]
 
 		#log maximum powers - cumulative
 		self.max_powers = np.maximum(power_level_ch, self.max_powers)
@@ -288,31 +300,34 @@ class _queue0_watcher(_threading.Thread):
 		periodic_max_powers_file = open(path_periodic_max_powers_log,'w')
 		np.save(periodic_max_powers_file, periodic_max_powers)
 
-		#Truncate before this point if necessary
+		#compute noise estimate (averaged)
 		min_power = np.amin (power_level_ch)
 		self.noise_estimate = (1-self.alpha_avg) * self.noise_estimate + self.alpha_avg * min_power
 		thr = self.noise_estimate * self.thr_leveler
 
-		# test detection threshold
+		#compare channel power with detection threshold
 		spectrum_constraint_hz = []
 		i = 0
 		for item in power_level_ch:
 			if item>thr:
-				spectrum_constraint_hz.append(ax_ch[i])
+				spectrum_constraint_hz.append(self.ax_ch[i])
 			i += 1
 
 		return spectrum_constraint_hz
 
 def src_power(psd, nFFT, Fr, Sf, bb_freqs, srch_bins):
+	#apply a moving average across psd - softens noise effect
 	psd = movingaverage(psd, 1*srch_bins)
-	fft_axis = Sf/2*np.linspace(-1, 1, nFFT) #fft_axis = np.fft.fftshift(f)
+	#fft_axis = Sf/2*np.linspace(-1, 1, nFFT) #fft_axis = np.fft.fftshift(f)
 	power_level_ch_fft = []
 
+	#compute power for left edge frequency
 	f = bb_freqs[0]
 	bin_n = (f+Sf/2)/Fr
 	power_level = float(sum(psd[0:int(bin_n+srch_bins/2)]))
 	power_level_ch_fft.append(power_level)
 
+	#compute power per frequency
 	for f in bb_freqs[1:]:
 		bin_n = (f+Sf/2)/Fr #freq = bin_n*Fr-Sf/2
 		power_level = float(sum(psd[int(bin_n-srch_bins/2):int(bin_n+srch_bins/2)]))
