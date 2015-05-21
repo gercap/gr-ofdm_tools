9877366b660c0fb852c484e3fe8101f954753e72
gercap <germanocapela@gmail.com>
2015-05-21 21:29:12 +0100

    coherence detector w/ bin estimator

--- python/coherence_detector.py
+++ python/coherence_detector.py
@@ -53,11 +53,10 @@ class coherence_detector(gr.hier_block2):
 		self.alpha_avg = alpha_avg #averaging factor for noise level between consecutive measurements
 		self.output = output
 		self.subject_channels = subject_channels
-		self.spectrum_constraint = []
+		self.subject_channels_outcome = []
 
 		#data queue to share data between theads
 		self.q0 = Queue.Queue()
-		self.q1 = Queue.Queue()
 
 		#gnuradio msg queues
 		self.msgq = gr.msg_queue(2)
@@ -69,29 +68,29 @@ class coherence_detector(gr.hier_block2):
 		self.connect(self, self.sink)
 
 		self._watcher = watcher(self.msgq, self.tune_freq, self.threshold, self.search_bw, self.N,
-		 self.sample_rate, self.q0, self.q1, self.subject_channels, self.set_spectrum_constraint)
-		self._output_data = output_data(self.q0, self.q1, self.sample_rate, self.tune_freq, self.N,
-			 self.search_bw, self.output, self.subject_channels)
+		 self.sample_rate, self.q0, self.subject_channels, self.set_subject_channels_outcome)
+		self._output_data = output_data(self.q0, self.sample_rate, self.tune_freq, self.N,
+			 self.search_bw, self.output, self.subject_channels, self.get_subject_channels_outcome)
 
-	def set_spectrum_constraint(self, spectrum_constraint):
-		self.spectrum_constraint = spectrum_constraint
+	def set_subject_channels_outcome(self, subject_channels_outcome):
+		self.subject_channels_outcome = subject_channels_outcome
 
-	def get_spectrum_constraint(self):
-		return self.spectrum_constraint
+	def get_subject_channels_outcome(self):
+		return self.subject_channels_outcome
 
 #ascii thread
 class output_data(_threading.Thread):
-	def __init__(self, data_queue0, data_queue1, sample_rate, tune_freq, N,
-	 search_bw, output, subject_channels):
+	def __init__(self, data_queue0, sample_rate, tune_freq, N,
+	 search_bw, output, subject_channels, get_subject_channels_outcome):
 		_threading.Thread.__init__(self)
 		self.setDaemon(1)
 		self.data_queue0 = data_queue0
-		self.data_queue1 = data_queue1
 		self.N = N
 		self.sample_rate = sample_rate
 		self.tune_freq = tune_freq
 		self.search_bw = search_bw
 		self.output = output
+		self.get_subject_channels_outcome = get_subject_channels_outcome
 
 		self.Fr = float(self.sample_rate)/float(self.N) #freq resolution
 		self.Fstart = self.tune_freq - self.sample_rate/2 #start freq
@@ -99,11 +98,11 @@ class output_data(_threading.Thread):
 		self.srch_bins = self.search_bw/self.Fr #binwidth for search
 		self.ax_ch = np.array(range(-self.N/2, self.N/2)) * self.Fr + self.tune_freq #subject channels
 
-		self.subject_channels = subject_channels # list of channels to be analysed by the flanck detector
-		self.idx_subject_channels = [0]*len(self.subject_channels) # aux list to index ax_ch
+		self.subject_channels = subject_channels
+		self.idx_subject_channels = [0]*len(self.subject_channels)
 		k = 0
 		for channel in subject_channels: 
-			self.idx_subject_channels[k] = self.ax_ch.index(channel)
+			self.idx_subject_channels[k] = find_nearest_index(self.ax_ch, channel)
 			k += 1
 
 		self.state = None
@@ -140,23 +139,30 @@ class output_data(_threading.Thread):
 
 		if self.output == 't_o':
 			while self.keep_running:
-				data = self.data_queue1.get()
+				print 'Freq [Hz]			Outcome'
+				print self.subject_channels
+				print self.get_subject_channels_outcome()
+				#for a, b in zip((self.subject_channels, self.get_subject_channels_outcome())):
+				#	print a, '			', b
+				print '\n'
+				sys.stdout.flush()
+				'''
 				left_column = np.array([['Freq [Hz]'],['Outcome']])
-				table0 = np.vstack((self.ax_ch, data))
+				table0 = np.vstack((self.subject_channels, self.get_subject_channels_outcome))
 				table =  np.hstack((left_column, table0))
 				table_plot = AsciiTable(np.ndarray.tolist(table.T))
 				print '\n'
 				print table_plot.table
 				print '\n'
 				sys.stdout.flush()
+				'''
 
 		if self.output == 'g_o':
 			while self.keep_running:
-				data = self.data_queue1.get()
 				self.gnuplot.stdin.write("set term dumb "+str(140)+" "+str(30)+ " \n")
 				self.gnuplot.stdin.write("plot '-' using 1:2 title 'Spectral Coherence Outcome' \n")
 
-				for i,j in zip(self.ax_ch, data):
+				for i,j in zip(self.subject_channels, self.get_subject_channels_outcome):
 					self.gnuplot.stdin.write("%f %f\n" % (i,j))
 
 				self.gnuplot.stdin.write("e\n")
@@ -168,7 +174,7 @@ class output_data(_threading.Thread):
 #queue wathcer to log statistics and max power per channel
 class watcher(_threading.Thread):
 	def __init__(self, rcvd_data, tune_freq, threshold,
-		 search_bw, N, sample_rate, data_queue0, data_queue1, subject_channels, set_spectrum_constraint):
+		 search_bw, N, sample_rate, data_queue0, subject_channels, set_subject_channels_outcome):
 		_threading.Thread.__init__(self)
 		self.setDaemon(1)
 		self.rcvd_data = rcvd_data
@@ -182,23 +188,27 @@ class watcher(_threading.Thread):
 		self.Fr = float(self.sample_rate)/float(self.N) #freq resolution
 		self.Fstart = self.tune_freq - self.sample_rate/2 #start freq
 		self.Ffinish = self.tune_freq + self.sample_rate/2 #end freq
-		self.srch_bins = self.search_bw/self.Fr #binwidth for search
-		self.ax_ch = np.array(range(-self.N/2, self.N/2)) * self.Fr + self.tune_freq #subject channels
+		self.srch_bins = int(self.search_bw/self.Fr/2) #binwidth for search
+		self.ax_ch = np.array(range(-self.N/2, self.N/2)) * self.Fr + self.tune_freq #all channels
+
+		# ax_ch - all channels available from bins
+		# subject_channels - channels to analyze
+		# idx_subject_channels - index CLOSEST subject_channels in ax_ch
+		# subject_channels_outcome - outcome for subject_channels
 
 		self.subject_channels = subject_channels
-		self.idx_subject_channels = [0]*len(self.subject_channels) # aux list to index ax_ch
+		self.n_chans = len(self.subject_channels)
+		self.idx_subject_channels = [0]*self.n_chans
 		k = 0
 		for channel in subject_channels: 
-			self.idx_subject_channels[k] = self.ax_ch.index(channel)
+			self.idx_subject_channels[k] = find_nearest_index(self.ax_ch, channel)
 			k += 1
-		#self.subject_channels_outcome = [True]*len(self.subject_channels)
-		self.outcome = [1]*len(self.ax_ch)
-		self.set_spectrum_constraint = set_spectrum_constraint
-		self.spectrum_constraint = []
+
+		self.set_subject_channels_outcome = set_subject_channels_outcome
+		self.subject_channels_outcome = [0]*self.n_chans
 
 		self.plc = np.array([0.0]*len(self.ax_ch))
 		self.data_queue0 = data_queue0
-		self.data_queue1 = data_queue1
 
 		self.keep_running = True
 		self.start()
@@ -225,16 +235,22 @@ class watcher(_threading.Thread):
 
 	#function that scans channels and compares with threshold to determine occupied / not occupied
 	def scanner(self, data):
-		self.spectrum_constraint = []
-		j = 0
-		for element in data:
-			if element > self.threshold:
-				self.outcome[j] = 1
-				self.spectrum_constraint.append(self.ax_ch[j])
+		self.subject_channels_outcome = [0]*self.n_chans
+
+		for j, channel in zip(range(self.n_chans),self.idx_subject_channels):
+			coherence = data[(channel-self.srch_bins):(channel+self.srch_bins)].sum()
+			print 'coherence', coherence
+			if coherence > self.threshold:
+				self.subject_channels_outcome[j] = coherence
 			else:
-				self.outcome[j] = 0
-			#print element, self.threshold
-			j += 1
-		print self.spectrum_constraint
-		self.set_spectrum_constraint(self.spectrum_constraint)
-		self.data_queue1.put(self.outcome)
+				self.subject_channels_outcome[j] = coherence
+
+		self.set_subject_channels_outcome(self.subject_channels_outcome)
+
+def find_nearest_index(array, value):
+	idx = (np.abs(array-value)).argmin()
+	return idx
+
+def find_nearest_value(array, value):
+	idx = (np.abs(array-value)).argmin()
+	return array[idx]
