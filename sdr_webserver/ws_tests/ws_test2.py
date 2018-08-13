d84e31ed4096d87de0ce4361ff867dd6a40e600d
gercap <germanocapela@gmail.com>
2018-08-13 20:50:31 +0100

    ws trials

---
+++ sdr_webserver/ws_tests/ws_test2.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import argparse
+import random
+import os
+
+import cherrypy
+
+from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
+from ws4py.websocket import WebSocket
+from ws4py.messaging import TextMessage
+
+from jinja2 import Environment, FileSystemLoader
+
+# GET CURRENT DIRECTORY
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+env=Environment(loader=FileSystemLoader(CUR_DIR), trim_blocks=True)
+
+class ChatWebSocketHandler(WebSocket):
+    def received_message(self, m):
+        cherrypy.engine.publish('websocket-broadcast', m)
+
+    def closed(self, code, reason="A client left the room without a proper explanation."):
+        cherrypy.engine.publish('websocket-broadcast', TextMessage(reason))
+
+class Root(object):
+    def __init__(self, host, port, ssl=False):
+        self.host = host
+        self.port = port
+        self.scheme = 'wss' if ssl else 'ws'
+
+    @cherrypy.expose
+    def index(self):
+
+      template = env.get_template('./index_ws.html')
+      return template.render(
+        username="User%d" % random.randint(0, 100),
+        host=self.host,
+        port=self.port,
+        scheme=self.scheme)
+
+    @cherrypy.expose
+    def ws(self):
+        cherrypy.log("Handler created: %s" % repr(cherrypy.request.ws_handler))
+
+if __name__ == '__main__':
+    import logging
+    from ws4py import configure_logger
+    configure_logger(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser(description='Echo CherryPy Server')
+    parser.add_argument('--host', default='127.0.0.1')
+    parser.add_argument('-p', '--port', default=9000, type=int)
+    parser.add_argument('--ssl', action='store_true')
+    args = parser.parse_args()
+
+    cherrypy.config.update({'server.socket_host': args.host,
+                            'server.socket_port': args.port,
+                            'tools.staticdir.root': os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))})
+
+    if args.ssl:
+        cherrypy.config.update({'server.ssl_certificate': './server.crt',
+                                'server.ssl_private_key': './server.key'})
+
+    WebSocketPlugin(cherrypy.engine).subscribe()
+    cherrypy.tools.websocket = WebSocketTool()
+
+    cherrypy.quickstart(Root(args.host, args.port, args.ssl), '', config={
+        '/ws': {
+            'tools.websocket.on': True,
+            'tools.websocket.handler_cls': ChatWebSocketHandler
+            },
+        '/js': {
+              'tools.staticdir.on': True,
+              'tools.staticdir.dir': 'js'
+            }
+        }
+)
